725514aa36f162d0df240efaf13a00ffd1bde819
budashan <152982018+budashan@users.noreply.github.com>
2023-12-05 22:56:14 +0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -130,7 +130,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Code Translator</title>
+        <title>Code Translator -GeFei</title>
         <meta
           name="description"
           content="Use AI to translate code from one language to another."
@@ -140,7 +140,7 @@ export default function Home() {
       </Head>
       <div className="flex h-full min-h-screen flex-col items-center bg-[#0E1117] px-4 pb-20 text-neutral-200 sm:px-10">
         <div className="mt-10 flex flex-col items-center justify-center sm:mt-20">
-          <div className="text-4xl font-bold">AI Code Translator</div>
+          <div className="text-4xl font-bold">GeFei-AI Code Translator</div>
         </div>
 
         <div className="mt-6 text-center text-sm">
